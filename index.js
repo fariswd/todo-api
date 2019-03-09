cea43333c6fec09461c64143a89b980560ee4629
Faris Widyantho <radenfaris@gmail.com>
2019-03-09 20:52:44 +0700

    add morgan & cors

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require('express')
 const app = express()
 
+const morgan = require('morgan')
+const cors = require('cors')
 const bodyParser = require('body-parser')
 
 const todoRoute = require('./routes/todoRoute')
 
+app.use(cors())
+app.use(morgan('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use('/', todoRoute)
 
-app.listen(3000, () => console.log('on port 3000'))
+app.listen(3000, () => console.log('running on port 3000'))
